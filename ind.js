624de58479bab18762310b9a475c50bb0694af8b
MINH THIEN <hatranminhthien@gmail.com>
2025-04-20 14:50:31 +0700

    Add files via upload

---
+++ ind.js
@@ -0,0 +1,13 @@
+function validateLogin() {
+    const username = document.querySelector('input[placeholder="Username"]').value.trim();
+    const password = document.querySelector('input[placeholder="Password"]').value.trim();
+
+    if (username === "" || password === "") {
+        alert("Bạn cần phải đăng nhập");
+        return false;
+    }
+
+    // Đăng nhập thành công, chuyển hướng
+    window.location.href = "index1.html";
+    return false;
+}
